b6456a3df33e0daf9752db3be6923953e11723e9
Shlok Sinha <shlokjp@gmail.com>
2024-01-13 23:19:05 +0530

    ep-02

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namaste React
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement (Refresh page)
+- File Watching Algorithm - in C++
+- Caching -> Faster Builds
+- Image Optimization
+- Minification of Files
+- Bundling of Files
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diganostic
+- Error Handling
+- Host on HTTPs
+- Tree Shaking - remove unused code
+- Different Dev & Prod Bundles
--- app.js
+++ app.js
@@ -1,21 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
   "div", // parent
   { id: "parent" },
-  [React.createElement( 
-    "div", // child
-    { id: "child1", key: 1 },
-    [      // grandchild - array of children
-      React.createElement("h1", {key: "1"}, "I am an H1 tag!"),
-      React.createElement("h2", {key: "2"}, "I am an H2 tag!"),
-    ]
-  ), React.createElement( 
-    "div", // child
-    { id: "child2", key: 2 },
-    [      // grandchild - array of children
-      React.createElement("h1", {key: "1"}, "I am an H1 tag!"),
-      React.createElement("h2", {key: "2"}, "I am an H2 tag!"),
-    ]
-  )]
+  [
+    React.createElement(
+      "div", // child
+      { id: "child1", key: 1 },
+      [
+        // grandchild - array of children
+        React.createElement("h1", { key: "1" }, "I am an H1 tag!"),
+        React.createElement("h2", { key: "2" }, "I am an H2 tag!"),
+      ]
+    ),
+    React.createElement(
+      "div", // child
+      { id: "child2", key: 2 },
+      [
+        // grandchild - array of children
+        React.createElement("h1", { key: "1" }, "I am an H1 tag!"),
+        React.createElement("h2", { key: "2" }, "I am an H2 tag!"),
+      ]
+    ),
+  ]
 );
 
 const heading = React.createElement(
