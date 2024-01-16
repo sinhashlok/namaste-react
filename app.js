de24ac40fbe3ab4a8a72b3d1d694d685f1c45f1e
Shlok Sinha <shlokjp@gmail.com>
2024-01-17 00:47:41 +0530

    ep-03

--- app.js
+++ app.js
@@ -1,37 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div", // parent
-  { id: "parent" },
-  [
-    React.createElement(
-      "div", // child
-      { id: "child1", key: 1 },
-      [
-        // grandchild - array of children
-        React.createElement("h1", { key: "1" }, "I am an H1 tag!"),
-        React.createElement("h2", { key: "2" }, "I am an H2 tag!"),
-      ]
-    ),
-    React.createElement(
-      "div", // child
-      { id: "child2", key: 2 },
-      [
-        // grandchild - array of children
-        React.createElement("h1", { key: "1" }, "I am an H1 tag!"),
-        React.createElement("h2", { key: "2" }, "I am an H2 tag!"),
-      ]
-    ),
-  ]
+// Functional Component
+const HeadingComponent = () => (
+  <div id="container">
+    <h1 id="heading">Hello World from React!</h1>
+    <Title />
+  </div>
 );
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Hello Namaste React 🙏!!
+  </h1>
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-root.render(parent);
-console.log(parent.props.children[0].props.children);
+
+root.render(<HeadingComponent />);
