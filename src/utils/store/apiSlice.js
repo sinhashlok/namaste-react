a7ebc8b80eb24d237b73cbe78a197a722eb1362c
Shlok Sinha <shlokjp@gmail.com>
2024-02-21 14:38:20 +0530

    cors issue in ApiSlice

--- src/utils/store/apiSlice.js
+++ src/utils/store/apiSlice.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://www.swiggy.com" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://corsproxy.io/?https://www.swiggy.com",
+  }),
   endpoints: (builder) => ({
     getRestaurants: builder.query({
       query: () =>
