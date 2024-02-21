3d23c04f3083a4801a80e5a6518410c5eeeb04a9
Shlok Sinha <shlokjp@gmail.com>
2024-02-21 14:49:14 +0530

    undo cors fix

--- src/utils/store/apiSlice.js
+++ src/utils/store/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://corsproxy.io/?https://www.swiggy.com",
+    baseUrl: "https://www.swiggy.com",
   }),
   endpoints: (builder) => ({
     getRestaurants: builder.query({
