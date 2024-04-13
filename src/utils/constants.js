102bf0d050409fb789c7581f0dd8be1c13188788
Shlok Sinha <shlokjp@gmail.com>
2024-04-14 01:51:55 +0530

    Updated CDN_URL to fetch Images of Resturant and Menu items

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const LOGO_URL =
   "https://cdn.vectorstock.com/i/preview-1x/73/82/food-logo-vector-38377382.jpg";
 
 export const CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const SWIGGY_API =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING";
